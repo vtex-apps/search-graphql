6ef32cc7a55e9d76dc8e8263c81ddab0bb25dc26
Jeymisson Oliveira <jeymissoncz@gmail.com>
2019-12-13 16:48:06 -0300

    Set indexing frequency to 500

---
+++ node/resolvers/stats/constants.ts
@@ -0,0 +1,9 @@
+export const VBASE_BUCKET = 'searchStats'
+export const URL_COUNT_FILE_PATH = 'searchUrlsCount.json'
+
+export const INDEXING_UPDATE_FREQUENCY = 500
+export const INDEXING_EVENT_ROUTE_NAME = 'searchUrlsCountIndex'
+
+export const MAP_QUERY_PARAM = 'map'
+export const PATH_SEPARATOR = '/'
+export const MAP_SEPARATOR = ','
