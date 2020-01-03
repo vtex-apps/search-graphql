1b467ed58b153879ceb8787377e62343341a27f6
Joao Fidelis <joaogfidelis@gmail.com>
2020-01-03 09:57:44 -0300

    stop using bluebird promise

--- node/resolvers/index.ts
+++ node/resolvers/index.ts
@@ -4,9 +4,6 @@ import {
   queries as searchQueries,
 } from './search'
 
-// eslint-disable-next-line no-global-assign
-Promise = require('bluebird')
-
 export const resolvers = {
   ...searchFieldResolvers,
   ...benefitsFieldResolvers,
