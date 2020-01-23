40c67a582b515b8516c4c66d2b9bdfb16ad686ec
Jeymisson Oliveira <jeymissoncz@gmail.com>
2020-01-23 19:27:03 -0300

    Surpress vbase save error

--- node/utils/vbase.ts
+++ node/utils/vbase.ts
@@ -37,6 +37,6 @@ const revalidate = async<T> (
   endDate: Date, validateFunction: (params?: any) => Promise<T>, params?: any) => {
   const data = await validateFunction(params)
   const revalidatedData = { data, ttl: endDate }
-  vbase.saveJSON<StaleRevalidateData<T>>(bucket, filePath, revalidatedData)
+  vbase.saveJSON<StaleRevalidateData<T>>(bucket, filePath, revalidatedData).catch()
   return data
 }
