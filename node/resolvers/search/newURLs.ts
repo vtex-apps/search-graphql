d0feed8bc9493e890e5ad46776dcdc6f36220d26
Jeymisson Oliveira <jeymissoncz@gmail.com>
2020-02-11 04:10:26 -0300

    Remove joinCategories logic to keep query order

--- node/resolvers/search/newURLs.ts
+++ node/resolvers/search/newURLs.ts
@@ -52,40 +52,13 @@ const mountCompatibilityQuery = async (params: {vbase: VBase, search: Search, ar
       compatQuerySegments.push(querySegment)
     }
   }
-
-  const { query: joinedQuerySegments, map: joinedCategoryMapSegments } = joinCategories(compatQuerySegments, compatMapSegments)
   
-  const compatibilityQuery = joinedQuerySegments.join('/')
-  const compatibilityMap = joinedCategoryMapSegments.join(',')
+  const compatibilityQuery = compatQuerySegments.join('/')
+  const compatibilityMap = compatMapSegments.join(',')
   return { query: compatibilityQuery, map: compatibilityMap}
 }
 
 const normalizeName = (name: string): string => searchSlugify(name)
-
-const joinCategories = (querySegments: string[], mapSegments: string[]) => {
-  const result : { query: string[], map: string[] } = { query: querySegments, map: mapSegments }
-  for (let i = 0; i < mapSegments.length - 1; i++){
-    const mapSegment = mapSegments[i]
-    const querySegment = querySegments[i]
-    const nextMapSegment = mapSegments[i + 1]
-    const nextQuerySegment = querySegments[i + 1]
-
-    const shouldShift = hasNoClusterIdAsFirstSegment(i, mapSegment) && hasCategoryMissplaced(mapSegment, nextMapSegment)
-
-    if(shouldShift){
-      result.map[i] = nextMapSegment
-      result.query[i] = nextQuerySegment
-      result.query[i+1] = querySegment
-      result.map[i+1] = mapSegment
-    }
-  }
-  return result
-}
-
-const hasCategoryMissplaced = (mapSegment: string, nextMapSegment: string): boolean => {
-  return (mapSegment !== CATEGORY_SEGMENT &&
-    nextMapSegment === CATEGORY_SEGMENT)
-}
 
 const getFacetsQueryFromCategories = (categories: (CategoryIdNamePair|null)[]) => {
   const queryArgs = categories.reduce((acc: QueryArgs, category) => {
