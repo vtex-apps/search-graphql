1dfd36054a721d5b933df526e71615f177d8b1e2
victorhmp <victor2142@gmail.com>
2020-02-17 20:54:01 -0300

    Add field resolver for giftSkuIds

--- node/resolvers/search/offer.ts
+++ node/resolvers/search/offer.ts
@@ -36,7 +36,45 @@ export const resolvers = {
       return [value]
     },
     teasers: propOr([], 'Teasers'),
-    giftSkuIds: propOr([], 'GiftSkuIds'),
+    giftSkuIds: ({ GiftSkuIds }: CommertialOffer, _: any, ctx: Context) => {
+      if (GiftSkuIds.length === 0) {
+        return []
+      }
+      const giftProducts = Promise.all(
+        GiftSkuIds.map(async skuId => {
+          const searchResult = await ctx.clients.search.productBySku([skuId])
+          const {
+            productName,
+            brand,
+            linkText,
+            description,
+            productTitle,
+            items,
+          } = searchResult[0]
+          const skuItem = items.find(item => item.itemId === skuId)
+
+          const productGiftProperties = {
+            productName,
+            brand,
+            linkText,
+            description,
+            productTitle,
+            skuItem: {
+              nameComplete: skuItem?.nameComplete ?? '',
+              images: skuItem?.images.map(({ imageLabel, imageUrl, imageText }) => ({
+                imageUrl,
+                imageLabel,
+                imageText,
+               })) ?? [],
+            },
+          }
+
+          return productGiftProperties
+        })
+      )
+
+      return giftProducts
+    },
     discountHighlights: propOr([], 'DiscountHighLight'),
   },
 }
